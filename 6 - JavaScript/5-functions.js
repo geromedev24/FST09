9038d8b0338f9bc66b7f20a2097488f5b97b81fa
geromedev24 <geromed1224@gmail.com>
2025-01-29 17:06:39 +0800

    Learned DOM Manipulation and Task Manager App

---
+++ 6 - JavaScript/5-functions.js
@@ -0,0 +1,24 @@
+/*
+Functions
+Reusable block of codes that can be executed whenever needed.
+*/ 
+function greet (){
+    console.log("Hello, welcome to the world of functions")
+    console.log("Let's go!")
+}
+
+// Calling a function
+greet ();
+greet ();
+greet ();
+
+// 2. Function definition with parameters.
+// parameters - placeholder
+// arguments - values that we pass to the functions parameters.
+function sum(num1, num2) {
+    return num1 + num2;
+  }
+  
+  console.log("Sum:", sum(10, 5));
+  console.log("Sum:", sum(29, 50));
+
--- 6 - JavaScript/app.js
+++ 6 - JavaScript/app.js
@@ -1,39 +1,37 @@
-// CFS: Control Flow Structures
+// Array - data structure that allows storing different variables
+let fruits = ["Apple", "Banana", "Orange", "Avocado", "Chico"];
+console.log("Fruits:", fruits);
+// Index specify the location of the element
+// 0,1,2,3
 
-// 1. Conditional Statements
+console.log("Length of Fruits:", fruits.length);
+console.log("First Fruit:", fruits[0]);
+console.log("Last Fruit:", fruits[3]);
+console.log("Last Fruit:", fruits[fruits.length - 1]);
 
-// Allow us to execute different blocks of code based on a condition.
-// if, else if, else statement
-// {} - Code Block
-// () - Conditional Block
+// Update elements
+fruits[0] = "Mango";
+console.log("Fruits:", fruits);
 
-let temperature = 29;
-if (temperature < 0) {
-  console.log("It's freezing!");
-  console.log("Stay indoors and drink hot chocolate.");
-} else if (temperature >= 0 && temperature <= 19) {
-  // Range: 0 - 19
-  console.log("It's cool outside.");
-} else {
-  console.log("It's warm outside.");
-}
+// property - an information about the object
+// methods - function that belongs to an object
+// Add Element
+// .push method - .push() add an element at the end of the array
 
-// 2. Looping Statements
-// Repeatedly execute a block of code until a specific condition is not met
+fruits.push("Appple", "Grapes")
+console.log("Fruits:", fruits);
 
-// For Loop
-/*
-    1st: Variable Initialization.
-    2nd: Condition Expression.
-    3rd: Increment/Decrement.
-*/
-for (let i = 1; i <= 3; i++) {
-  console.log("For Loop Count:", i);
-}
+// .pop() - delete an element at the end of the array
+let deletedFruits = fruits.pop();
+console.log("Deleted Fruits:", deletedFruits);
+console.log("Fruits:", fruits);
 
-// While Loop
-let count = 1;
-while (count <= 3) {
-  console.log("While Loop Count:", count);
-  count++;
-}
+// .includes(): check if an element exists
+// Return: either true or false
+// includes method is case sensitive
+console.log("Is Orange inside of fruits:", fruits.includes("Orange"));
+
+fruits.forEach(function(fruit){
+    console.log("Fruit:", fruit);
+});
+
